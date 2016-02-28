b7382c90c8555d298b105c648ec4705bde6fcfb0
megkthrock <megkthrock@gmail.com>
2016-02-28 11:50:56 -0500

    Remove Dashes from ISBN

--- ISBN.rb
+++ ISBN.rb
@@ -2,3 +2,11 @@ def isbn_length(any_string)
 	any_string.length == 10 or any_string.length ==13
 
 end
+
+def remove_dashes(any_string)
+	dashes = "-"
+	if any_string.include? dashes
+		any_string.delete! dashes
+	
+	end
+end
--- ISBNTest.rb
+++ ISBNTest.rb
@@ -5,7 +5,15 @@ class Test_ISBN_Number <Minitest::Test
 
 	def test_isbn_length
 		assert_equal(false, isbn_length("04719586970")) 
+		assert_equal(false, isbn_length(""))
+	end
+
+	def test_length_is_10
+		assert_equal(true, isbn_length("0471958697"))
+	end
+
+	def test_for_no_dashes
+		assert_equal("9780471486480", remove_dashes("978-0-471-48648-0"))
 	end
 end
 
- 
